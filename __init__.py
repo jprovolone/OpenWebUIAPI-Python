488424192989117b884ef51b987313f7f55ae383
jprovost <jprovost@icthomasson.com>
2024-12-02 13:21:25 -0600

    add setup.py and init.py

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# your_library_name/__init__.py
+from .openwebui_api import OpenWebUI
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='openwebui_python',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    description='A Python client for interacting with OpenWebUI\'s API, providing easy access to language models and chat completions.',
+    author='John Provost',
+    author_email='john@johnprovost.com',
+    url='https://github.com/jprovolone/OpenWebUIAPI-Python',
+)
